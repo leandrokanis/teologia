b8a4d288807533eef590879ce58fedab502bf6a1
Leandro Alves <leandrosustenido@gmail.com>
2020-05-28 15:06:16 -0300

    :tada: Initial commit

---
+++ .vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: 'Christian System',
+    description: 'A Christian System, by Alexander Campbell',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+            { text: 'Config Page', link: '/config' },
+        ],
+        sidebar: 'auto'
+    },
+}
